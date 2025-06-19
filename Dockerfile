8ef902952b2c1343845197e0d905bceb58ee51af
Michael Kreil <github@michael-kreil.de>
2025-06-19 16:37:38 +0200

    fix: run script

--- Dockerfile
+++ Dockerfile
@@ -7,11 +7,11 @@ RUN set -eux; \
     apt-get update -qq; \
     apt-get install -yq --no-install-recommends openjdk-21-jdk curl sudo; \
     curl -Ls "https://github.com/onthegomap/planetiler/releases/latest/download/planetiler.jar" -o planetiler.jar; \
-    curl -Ls "https://raw.githubusercontent.com/versatiles-org/versatiles-rs/main/helpers/install-unix.sh" | bash; \
+    curl -Ls "https://github.com/versatiles-org/versatiles-rs/raw/main/scripts/install-unix.sh" | sudo sh; \
     apt-get purge -y --auto-remove curl sudo || true; \
     apt-get clean; \
     rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
-COPY ressources .
+COPY resources .
 
-CMD ["bash", "./run.sh"]
+ENTRYPOINT ["bash", "./run.sh"]
--- resources/run.sh
+++ resources/run.sh
@@ -1,17 +1,73 @@
 #!/bin/bash
 
+display_help() {
+	echo "Options:"
+	echo "  -h, --help          Show this help message"
+	echo "  -a, --area <name>   Specify the area to process (e.g.: \"planet\", \"geofabrik:ukraine\", etc.)"
+}
+
+AREA=""
+SOURCE="osm"
+
+while [[ $# -gt 0 ]]; do
+	case $1 in
+	-h | --help)
+		display_help
+		exit 0
+		;;
+	-a | --area)
+		AREA="$2"
+		shift # past argument
+		shift # past value
+		;;
+	-* | --* | *)
+		echo "Unknown option $1"
+		exit 1
+		;;
+	esac
+done
+
+if [[ -z "$AREA" ]]; then
+	echo "Error: Area must be specified with -a or --area option."
+	display_help
+	exit 1
+fi
+
+NAME=$SOURCE
+
+if [[ "$AREA" != "planet" ]]; then
+	NAME=$NAME-${AREA##*:}
+fi
+
 # get the available memory
 available_memory=$(free -m | awk '/^Mem:/{print $7}')
 
 if [ "$available_memory" -lt 4096 ]; then
-	# reserve 25% of the available memory
+	# use 75% of the available memory for processing
 	available_memory=$(awk '{print int($1 * 0.75)}' <<<"${available_memory}")
 else
 	# reserve 1GB for the system and other processes
 	available_memory=$((available_memory - 1024))
 fi
 
-# java -Xmx"${available_memory}"m -jar planetiler.jar --download --area=berlin --output=data/berlin.pmtiles
+if [ "$available_memory" -gt 130000 ]; then
+	STORAGE="ram"
+else
+	STORAGE="mmap"
+fi
 
+java -Xmx"${available_memory}"m -jar planetiler.jar \
+	--area=$AREA \
+	--download \
+	--download-threads=10 \
+	--download-chunk-size-mb=1000 \
+	--fetch-wikidata \
+	--nodemap-type=sparsearray \
+	--storage=$STORAGE \
+	--output=data/$NAME.pmtiles \
+	--force=true \
+	config/shortbread.yml
 
-java -Xmx"${available_memory}"m -jar planetiler.jar --help
+versatiles convert -c brotli data/$NAME.pmtiles data/$NAME.versatiles
+
+# java -Xmx"${available_memory}"m -jar planetiler.jar --help
